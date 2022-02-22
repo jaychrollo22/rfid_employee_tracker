0635f7fcf17b84546a8614e22b32f9af8c57a3ce
Arjay Lumagdong <arjay.lumagdong@lfuggoc.net>
2022-02-22 16:31:26 +0800

    update rfid employee tracker

--- resources/views/layouts/app_main.blade.php
+++ resources/views/layouts/app_main.blade.php
@@ -89,7 +89,7 @@
         </span>
     </div>
    
-    <script src="{{ asset('js/all.js?v=1.0.4') }}" defer></script>
+    <script src="{{ asset('js/all.js?v=1.0.5') }}" defer></script>
 
     <!--begin::Global Theme Bundle(used by all pages)-->
     <script src="assets/plugins/global/plugins.bundle.js"></script>
